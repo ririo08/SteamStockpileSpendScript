aeda8022234060c657181b01e9a1b981c93ae537
ririo <ririomail@gmail.com>
2019-07-29 21:57:45 +0900

    2nd

--- randomscript.js
+++ randomscript.js
@@ -13,15 +13,15 @@ javascript: (
     }
 )();
 
-javascript: (
-    function () {
-        classCount = document.getElementsByClassName('gameListRow').length;
-        var random = Math.floor(Math.random() * classCount) + 1;
-        var gameList = document.querySelectorAll('.gameListRow');
-        gameList[random].id = 'thisgameiscool';
-        document.getElementById('thisgameiscool').style.backgroundColor = '#2d1616';
-        window.location.hash = '';
-        window.location.hash = 'thisgameiscool';
-    }
-)();
-javascript:(function(){classCount=document.getElementsByClassName('gameListRow').length;var a=Math.floor(Math.random()*classCount)+1;document.querySelectorAll('.gameListRow')[a].id='thisgameiscool';document.getElementById('thisgameiscool').style.backgroundColor='#2d1616';window.location.hash='';window.location.hash='thisgameiscool'})();
+// javascript: (
+//     function () {
+//         classCount = document.getElementsByClassName('gameListRow').length;
+//         var random = Math.floor(Math.random() * classCount) + 1;
+//         var gameList = document.querySelectorAll('.gameListRow');
+//         gameList[random].id = 'thisgameiscool';
+//         document.getElementById('thisgameiscool').style.backgroundColor = '#2d1616';
+//         window.location.hash = '';
+//         window.location.hash = 'thisgameiscool';
+//     }
+// )();
+// javascript:(function(){classCount=document.getElementsByClassName('gameListRow').length;var a=Math.floor(Math.random()*classCount)+1;document.querySelectorAll('.gameListRow')[a].id='thisgameiscool';document.getElementById('thisgameiscool').style.backgroundColor='#2d1616';window.location.hash='';window.location.hash='thisgameiscool'})();
