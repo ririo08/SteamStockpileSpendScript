b7fc1922c9dff8f6f51e5b803713a59ad47256c2
ririo <ririomail@gmail.com>
2019-09-29 23:52:35 +0900

    add tweetcopy script

--- TweetCopy/tweetcopy.js
+++ TweetCopy/tweetcopy.js
@@ -1,9 +1,14 @@
+// ツイート内容を取得
 var originalTweet = document.getElementById('status');
+
+// 指定の形式に書き換え
 var tweet = originalTweet.value.replace( / https/g, '\nhttps');
 tweet = tweet.replace( /@YouTubeさんから/g, '');
 tweet = '動画投稿 ▼\n\n' + tweet;
+
+// 書き換えたツイートをエリアに出力
 originalTweet.value = tweet;
+
+//エリアを選択してコピー
 originalTweet.select();
 document.execCommand('copy');
-
-javascript: (function ()% 7Bvar % 20originalTweet % 20 % 3D % 20document.getElementById('status') % 3Bvar % 20tweet % 20 % 3D % 20originalTweet.value.replace(% 20 % 2F % 20https % 2Fg % 2C % 20'%5Cnhttps') % 3Btweet % 20 % 3D % 20tweet.replace(% 20 % 2F % 40YouTube % E3 % 81 % 95 % E3 % 82 % 93 % E3 % 81 % 8B % E3 % 82 % 89 % 2Fg % 2C % 20'') % 3Btweet % 20 % 3D % 20'%E5%8B%95%E7%94%BB%E6%8A%95%E7%A8%BF%20%E2%96%BC%5Cn%5Cn' % 20 % 2B % 20tweet % 3BoriginalTweet.value % 20 % 3D % 20tweet % 3BoriginalTweet.select() % 3Bdocument.execCommand('copy') % 7D) ()
