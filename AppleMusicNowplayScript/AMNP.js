6463477222a3fb7a00d5a465115280996ff62032
ririo <ririomail@gmail.com>
2019-10-08 14:18:29 +0900

    update AMNP.js

--- AppleMusicNowplayScript/AMNP.js
+++ AppleMusicNowplayScript/AMNP.js
@@ -2,4 +2,5 @@ let songName = document.getElementById('apple-music-player').title;
 let shareURL = 'https://twitter.com/share?text=';
 let shareHash = ' #nowplaying';
 shareURL += encodeURIComponent(songName) + encodeURIComponent(shareHash) +'&url=none';
+window.open(shareURL, '_blank');
 console.log(shareURL);
