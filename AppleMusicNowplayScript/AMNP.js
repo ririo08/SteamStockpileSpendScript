8f5ebf3cbc30838a6d845c04e1a0dcabf168db6f
ririo08 <ririomail@gmail.com>
2019-10-10 12:58:23 +0900

    add detail AMNP and delete SRR.min.js

--- AppleMusicNowplayScript/AMNP.js
+++ AppleMusicNowplayScript/AMNP.js
@@ -1,6 +1,15 @@
+// 再生中の曲を取得
 let songName = document.getElementById('apple-music-player').title;
+
+// ツイート用のURLを生成
 let shareURL = 'https://twitter.com/share?text=';
+
+// Nowplayingも忘れずに。
 let shareHash = ' #nowplaying';
+
+// URLをencodeURIComponentで使える形式に変換
 shareURL += encodeURIComponent(songName) + encodeURIComponent(shareHash) +'&url=none';
+
+// 共有ページを開く
 window.open(shareURL, '_blank');
 console.log(shareURL);
