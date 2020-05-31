4a53c61346e4c3fb0e05d5c149dad450317646eb
ririo <ririomail@gmail.com>
2020-06-01 01:17:05 +0900

    add miterunow.js

---
+++ miterunow/miterunow.js
@@ -0,0 +1,8 @@
+javascript:(
+    function(){
+        let url = location.href;
+        let title = encodeURIComponent(document.title);
+        let tweetURL = `https://twitter.com/intent/tweet?url=${url}&text=${title}&hashtags=miteru`;
+        window.open(tweetURL, '_blank');
+    }
+)();
