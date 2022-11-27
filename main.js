553df6dee18437bc2855d40b97f65dd7090f7e6f
ririo08 <ririomail@gmail.com>
2022-11-27 14:37:48 +0900

    feat: github pages用のdist出力を追加

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 import { bookmarkletter } from "bookmarkletter"
-import * as fs from "fs"
+import fs from "fs-extra"
+import { marked } from "marked"
 
-const src = fs.readFileSync("src/AppleMusicNowplayScript/src.js", "utf-8")
-const res = bookmarkletter(src)
-console.log(res)
-// console.log(fs.readFileSync("docs/index.html", "utf-8"))
+// ファイルパス取得
+const filepath = fs.readdirSync("src")
+
+// ソース取得
+const srcs = []
+for (const item of filepath) {
+  const src = fs.readFileSync(`src/${item}/src.js`, "utf-8")
+  const res = bookmarkletter(src)
+  srcs.push(res)
+}
+
+// README.md取得
+const docs = []
+for (const item of filepath) {
+  const src = marked.parse(fs.readFileSync(`src/${item}/README.md`, "utf-8")).replace('<a', '<a class="bookmarklet"')
+  docs.push(src)
+}
+
+// ドキュメント生成
+let doc = ""
+docs.forEach((v, i) => {
+  doc += '<div class="wrapper">'
+  doc += v.replace("TARGET", srcs[i])
+  doc += '</div>'
+})
+
+let html = fs.readFileSync("docs/index.html", "utf-8")
+const res = html.replace("<!-- TARGET -->", doc)
+
+fs.mkdirsSync("dist");
+fs.copySync("docs/css/", "dist/css/")
+fs.writeFileSync("dist/index.html", res.replace(/\s{2}/g, '').replace(/\n/g, ''))
 
