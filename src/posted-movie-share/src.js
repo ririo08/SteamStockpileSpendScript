1d655906a0568a77beaef53f34121b59a311f899
ririo08 <ririomail@gmail.com>
2022-11-29 02:07:09 +0900

    fix: 動画投稿用ツイートスクリプトを動くように

--- src/posted-movie-share/src.js
+++ src/posted-movie-share/src.js
@@ -1,20 +1,11 @@
-// Youtubeの再生ページからビデオIDを取得
-let videoID = window.location.search.split('v=')[1];
-let idPosition = videoID.indexOf('&');
-if (idPosition != -1) {
-    videoID = videoID.substring(0, idPosition);
-}
+// 情報取得
+const videoID = document.querySelector('meta[itemprop="videoId"]').content
+const videoTitle = document.querySelector('meta[name="title"]').content
+const videoURL = document.querySelector('link[rel="shortlinkUrl"]').href
 
-// 動画URL生成
-let videoURL = "https://youtu.be/" + videoID;
-
-// タイトルを取得
-let videoTitle = document.querySelector("h1.style-scope.ytd-video-primary-info-renderer").textContent
-videoTitle = encodeURIComponent(videoTitle);
-
-// ツイートを特定形式に変換
-let tweetText = `%E5%8B%95%E7%94%BB%E6%8A%95%E7%A8%BF%E2%96%BC%0D%0A%0D%0A${videoTitle}%0D%0A`;
+// ツイート文作成
+const tweetText = `動画投稿▼%0a%0a${videoTitle}`;
 
 // URLを作成
-let tweetURL = `https://twitter.com/intent/tweet?url=${videoURL}&text=${tweetText}`;
+const tweetURL = `https://twitter.com/intent/tweet?url=${videoURL}&text=${tweetText}`;
 window.open(tweetURL, '_blank');
